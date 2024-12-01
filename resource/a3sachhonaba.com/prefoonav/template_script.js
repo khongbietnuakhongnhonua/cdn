cce9417c65dd9beee87e9cbdb6d641de01ed9a73
nmkdev <nguyenminhkhoi.nmk.mail@gmail.com>
2024-12-01 18:33:53 +0700

    Update template_script.js

--- resource/a3sachhonaba.com/prefoonav/template_script.js
+++ resource/a3sachhonaba.com/prefoonav/template_script.js
@@ -4,6 +4,7 @@ function addVersionToURL(url) {
 }
 
 function loadPreloader() {
+    console.log("Loading preloader...");
     fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/preloader.html'))
         .then(response => response.text())
         .then(data => {
@@ -12,6 +13,7 @@ function loadPreloader() {
             link.rel = 'stylesheet';
             link.href = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/preloader.css');
             document.head.appendChild(link);
+            console.log("Preloader loaded.");
         })
         .catch(error => {
             console.error('Error loading preloader:', error);
@@ -19,6 +21,7 @@ function loadPreloader() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOMContentLoaded event fired.");
     loadPreloader();
     
     fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.html'))
@@ -28,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const script = document.createElement('script');
             script.src = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.js');
             document.body.appendChild(script);
+            console.log("Navbar loaded.");
         })
         .catch(error => {
             console.error('Error loading navbar:', error);
@@ -41,6 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
             link.rel = 'stylesheet';
             link.href = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/footer.css');
             document.head.appendChild(link);
+            console.log("Footer loaded.");
         })
         .catch(error => {
             console.error('Error loading footer:', error);
@@ -53,5 +58,7 @@ window.addEventListener("load", function() {
     if (loader) {
         loader.style.display = "none";
         console.log("Done!");
+    } else {
+        console.log("Preloader element not found.");
     }
 });
