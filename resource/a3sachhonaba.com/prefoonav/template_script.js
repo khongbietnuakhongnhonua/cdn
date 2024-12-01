c9c86d634af98039d326f39390f62228042e8bb3
nmkdev <nguyenminhkhoi.nmk.mail@gmail.com>
2024-12-01 19:03:45 +0700

    sd

--- resource/a3sachhonaba.com/prefoonav/template_script.js
+++ resource/a3sachhonaba.com/prefoonav/template_script.js
@@ -15,7 +15,7 @@ function addVersionToURL(url) {
 
 function loadPreloader() {
     console.log("Loading preloader...");
-    fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/preloader.html'))
+    return fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/preloader.html'))
         .then(response => response.text())
         .then(data => {
             document.getElementById('preloader-container').innerHTML = data;
@@ -32,37 +32,37 @@ function loadPreloader() {
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOMContentLoaded event fired.");
-    loadPreloader();
-    
-    fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.html'))
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-container').innerHTML = data;
-            const script = document.createElement('script');
-            script.src = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.js');
-            document.body.appendChild(script);
-            console.log("Navbar loaded.");
-        })
-        .catch(error => {
-            console.error('Error loading navbar:', error);
+    loadPreloader().then(() => {
+        return fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.html'))
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+                const script = document.createElement('script');
+                script.src = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/navbar.js');
+                document.body.appendChild(script);
+                console.log("Navbar loaded.");
+            })
+            .catch(error => {
+                console.error('Error loading navbar:', error);
+            });
+    }).then(() => {
+        return fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/footer.html'))
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer-container').innerHTML = data;
+                const link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/footer.css');
+                document.head.appendChild(link);
+                console.log("Footer loaded.");
+            })
+            .catch(error => {
+                console.error('Error loading footer:', error);
+            });
+    }).then(() => {
+        window.addEventListener('load', function() {
+            console.log("Window loaded.");
+            hideloader();
         });
-
-    fetch(addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/footer.html'))
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data; // Nhúng nội dung vào footer-container
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = addVersionToURL('https://cdn.a3sachhonaba.com/resource/a3sachhonaba.com/prefoonav/footer.css');
-            document.head.appendChild(link);
-            console.log("Footer loaded.");
-        })
-        .catch(error => {
-            console.error('Error loading footer:', error);
-        });
-});
-
-window.addEventListener('load', function() {
-    console.log("Window loaded.");
-    hideloader(); // Chỉ gọi hideLoader sau khi toàn bộ trang đã tải xong
+    });
 });
