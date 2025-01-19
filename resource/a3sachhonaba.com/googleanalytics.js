7e146888690928681e6a980f7e40f67c3d59289e
Nguyễn Minh Khôi <nguyenminhkhoi2009.nmk@gmail.com>
2025-01-19 19:14:04 +0700

    Thêm tí analytics.js

---
+++ resource/a3sachhonaba.com/googleanalytics.js
@@ -0,0 +1,12 @@
+(function() {
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-1MF3YV1Y2L';
+    document.head.appendChild(gtagScript);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-1MF3YV1Y2L');
+})();
